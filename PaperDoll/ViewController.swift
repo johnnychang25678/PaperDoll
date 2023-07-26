a020aa7041e19e671a387416d12362a32c20d761
johnnychang25678 <zjps3407@gmail.com>
2023-07-26 18:26:57 +0800

    Initial Commit

---
+++ PaperDoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaperDoll
+//
+//  Created by Chia-ming Chang on 2023/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
